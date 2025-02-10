967d6a9055922eeb0ee4fdbf26cd71fbd196c57b
Tom Kamberg-Buhrtz <buhrtz@melag.de>
2025-02-10 23:15:16 +0100

    Set up CoreServiceLocator CMakeLists with library targets and includes

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ pushd "$dir" > /dev/null || exit 2
 #mkdir -p build/ && cd build && cmake .. && make -j
 
 
-rm -rf build/ && mkdir -p build/ && cd build && cmake -GNinja -DCMAKE_TOOLCHAIN_FILE=toolchains/clang-toolchain.cmake .. && ninja -v -j $(nproc)
+rm -rf build/ && mkdir -p build/ && cd build && cmake -GNinja -DCMAKE_INSTALL_PREFIX=deploy -DCMAKE_TOOLCHAIN_FILE=toolchains/clang-toolchain.cmake .. && ninja -v -j $(nproc) install
 
 # MODULES DO NOT WORK CORRECTLY WITH g++-14 
 #rm -rf build/ && mkdir -p build/ && cd build && cmake -GNinja -DCMAKE_TOOLCHAIN_FILE=toolchains/gcc14-toolchain.cmake .. && ninja -v -j $(nproc)
