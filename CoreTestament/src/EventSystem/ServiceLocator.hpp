563afa0a431c1205804780cafba35fa9a17f49d2
ToKaBu <tom.buhrtz@gmail.com>
2026-06-26 14:06:47 +0200

    Remove commented-out EventManager-based ServiceLocator alternative

--- CoreTestament/src/EventSystem/ServiceLocator.hpp
+++ CoreTestament/src/EventSystem/ServiceLocator.hpp
@@ -68,30 +68,4 @@ public:
     }
 };
 
-#endif // SERVICE_LOCATOR_HPP
-
-
-/*#ifndef EVENTSYSTEM_SERVICELOCATOR_HPP
-#define EVENTSYSTEM_SERVICELOCATOR_HPP
-
-
-#include "EventSystem/EventManager.hpp"
-
-class ServiceLocator {
-public:
-    static void provide(EventManager* manager) {
-        eventManager = manager;
-    }
-
-    static EventManager& getEventManager() {
-        if (!eventManager) {
-            throw std::runtime_error("EventManager not provided!");
-        }
-        return *eventManager;
-    }
-
-private:
-    static inline EventManager* eventManager = nullptr;
-};
-
-#endif*/
+#endif
